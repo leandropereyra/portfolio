4e690d8c8d61be06f08aedc40e6deca40d1cee28
Gastón Leandro Pereyra <leandro-pereyra@hotmail.es>
2022-06-26 14:04:25 +0200

    Nueva tarjeta TodoList

--- src/components/ArrayCards.js
+++ src/components/ArrayCards.js
@@ -2,16 +2,19 @@ import celia from "../assets/img/web/celiamartinperez.jpg";
 import yoga from "../assets/img/web/yogaypilatesoviedo.jpg";
 import alicia from "../assets/img/web/elmenudealicia.jpg";
 import leandro from "../assets/img/web/leandropereyra.jpg";
+import todo from '../assets/img/web/listatareas.jpg'
 
 import { IoLogoWordpress } from "react-icons/io";
 import { FaReact } from "react-icons/fa";
 import { SiTailwindcss } from "react-icons/si";
 import { SiGithub } from "react-icons/si";
+import { SiJavascript } from "react-icons/si";
 
-const wordpress = <IoLogoWordpress className="text-6xl text-sky-500" />;
-const react = <FaReact className="text-6xl text-sky-500" />;
-const tailwind = <SiTailwindcss className="text-6xl text-sky-500" />;
-const github = <SiGithub className="text-6xl text-black" />;
+const wordpress = <IoLogoWordpress className="text-4xl md:text-6xl text-sky-500" />;
+const react = <FaReact className="text-4xl md:text-6xl text-sky-500" />;
+const tailwind = <SiTailwindcss className="text-4xl md:text-6xl text-sky-500" />;
+const github = <SiGithub className="text-4xl md:text-6xl text-black" />;
+const javascript = <SiJavascript className="text-4xl md:text-6xl text-yellow-500" />;
 
 export const ArrayCards = [
   {
@@ -40,6 +43,13 @@ export const ArrayCards = [
     title: "LeandroPereyra.com",
     image: leandro,
     skill: [react, tailwind, github],
+    github: "https://github.com/leandropereyra/portfolio",
+  },
+  {
+    id: 5,
+    title: "Lista de Tareas CRUD",
+    image: todo,
+    skill: [react, tailwind, github, javascript],
     github: "https://github.com/leandropereyra/portfolio",
   },
 ];
