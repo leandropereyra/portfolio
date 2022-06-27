ebfb8e511360593d1b27f8d17822b7ad84f7c7ea
Gastón Leandro Pereyra <leandro-pereyra@hotmail.es>
2022-06-27 15:10:31 +0200

    Weather APP

--- src/components/ArrayCards.js
+++ src/components/ArrayCards.js
@@ -3,17 +3,16 @@ import yoga from "../assets/img/web/yogaypilatesoviedo.jpg";
 import alicia from "../assets/img/web/elmenudealicia.jpg";
 import leandro from "../assets/img/web/leandropereyra.jpg";
 import todo from '../assets/img/web/listatareas.jpg'
+import weather from '../assets/img/web/weather.jpg'
 
 import { IoLogoWordpress } from "react-icons/io";
 import { FaReact } from "react-icons/fa";
 import { SiTailwindcss } from "react-icons/si";
-import { SiGithub } from "react-icons/si";
 import { SiJavascript } from "react-icons/si";
 
 const wordpress = <IoLogoWordpress className="text-4xl md:text-6xl text-sky-500" />;
 const react = <FaReact className="text-4xl md:text-6xl text-sky-500" />;
 const tailwind = <SiTailwindcss className="text-4xl md:text-6xl text-sky-500" />;
-const github = <SiGithub className="text-4xl md:text-6xl text-black" />;
 const javascript = <SiJavascript className="text-4xl md:text-6xl text-yellow-500" />;
 
 export const ArrayCards = [
@@ -42,15 +41,23 @@ export const ArrayCards = [
     id: 4,
     title: "LeandroPereyra.com",
     image: leandro,
-    skill: [react, tailwind, github],
+    skill: [react, tailwind,],
     github: "https://github.com/leandropereyra/portfolio",
   },
   {
     id: 5,
     title: "Lista de Tareas CRUD",
     image: todo,
-    skill: [react, tailwind, github, javascript],
+    skill: [react, tailwind, javascript],
     github: "https://github.com/leandropereyra/todolist",
     url: "https://leandropereyra.github.io/todolist/"
+  },
+  {
+    id: 6,
+    title: "Weather APP",
+    image: weather,
+    skill: [react, tailwind, javascript],
+    github: "https://github.com/leandropereyra/weatherapp",
+    url: "https://leandropereyra.github.io/weatherapp/"
   },
 ];
